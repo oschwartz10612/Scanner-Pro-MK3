423e1e09b102d58fc9f4539de94170823e92626e
Owen Schwartz <oschwartz10612@gmail.com>
2019-08-17 14:47:04 -0400

    created installer

--- Scanner Pro MK3 Uploader/upload.bat
+++ Scanner Pro MK3 Uploader/upload.bat
@@ -21,7 +21,7 @@ CHOICE /C 1234567 /M "Enter your choice: "
 IF ERRORLEVEL 7 GOTO racklocationLOANER
 IF ERRORLEVEL 6 GOTO racklocationFCPSON
 IF ERRORLEVEL 5 GOTO rackLocation
-IF ERRORLEVEL 4 GOTO pxiID_FCPSON
+IF ERRORLEVEL 4 GOTO pxi_FCPSON
 IF ERRORLEVEL 3 GOTO pxiID_2390N
 IF ERRORLEVEL 2 GOTO pxiID_2390D
 IF ERRORLEVEL 1 GOTO distribution
@@ -29,50 +29,43 @@ IF ERRORLEVEL 1 GOTO distribution
 :distribution
 ECHO.
 ECHO 1. Distribution
-cd .\avrdude
-.\uploader.exe %port% ..\scripts\distribution.hex
+.\32u4Upload.exe %port% .\distribution.hex
 GOTO End
 
 :pxiID_2390D
 ECHO.
 ECHO 2. PXI ID + Barcode (2390D)
-cd .\avrdude
-.\uploader.exe %port% ..\scripts\pxiID_2390D.hex
+.\32u4Upload.exe %port% .\pxiID_2390D.hex
 GOTO End
 
 :pxiID_2390N
 ECHO.
 ECHO 3. PXI ID + Barcode (2390N)
-cd .\avrdude
-.\uploader.exe %port% ..\scripts\pxiID_2390N.hex
+.\32u4Upload.exe %port% .\pxiID_2390N.hex
 GOTO End
 
-:pxiID_FCPSON
+:pxi_FCPSON
 ECHO.
 ECHO 4. PXI ID + FCPSON
-cd .\avrdude
-.\uploader.exe %port% ..\scripts\pxi_FCPSON.hex
+.\32u4Upload.exe %port% .\pxi_FCPSON.hex
 GOTO End
 
 :rackLocation
 ECHO.
 ECHO 5. Rack Scanning Location
-cd .\avrdude
-.\uploader.exe %port% ..\scripts\rackLocation.hex
+.\32u4Upload.exe %port% .\rackLocation.hex
 GOTO End
 
 :racklocationFCPSON
 ECHO.
 ECHO 6. Rack Scanning Location + FCPSON
-cd .\avrdude
-.\uploader.exe %port% ..\scripts\racklocationFCPSON.hex
+.\32u4Upload.exe %port% .\racklocationFCPSON.hex
 GOTO End
 
 :racklocationLOANER
 ECHO.
 ECHO 7. Rack Scanning Location + LOANER
-cd .\avrdude
-.\uploader.exe %port% ..\scripts\racklocationLOANER.hex
+.\32u4Upload.exe %port% .\racklocationLOANER.hex
 GOTO End
 
 :End
