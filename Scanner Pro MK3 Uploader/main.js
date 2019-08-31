c9fef4ce8533d2825c9869e96359ec7a61b9c338
Owen Schwartz <oschwartz10612@gmail.com>
2019-08-31 10:09:48 -0400

    added serialport control

--- Scanner Pro MK3 Uploader/main.js
+++ Scanner Pro MK3 Uploader/main.js
@@ -5,13 +5,13 @@ var spawn = require('child_process').spawn
 
 let win
 let file
-let comPort   
+let comPort
 
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 600,
-    height: 500,
+    width: 1000,
+    height: 1000,
     webPreferences: {
       nodeIntegration: true
     }
@@ -67,23 +67,65 @@ ipc.on('open-file-dialog-for-file', function (event) {
    comPort = data
  })
 
- ipc.on('run', function (event, data) {
-  runAvrdude(comPort, file)
- })
-
-function runAvrdude(port, file) {
-  avrdude = spawn('./avrdude/32u4Upload.exe', [port, file]);
-
-  avrdude.stdout.on('data', function (data) {
-    win.webContents.send('avrdude', data.toString());
+var serialport = require("serialport");
+ipc.on('run', function (event, data) {
+  serialport.list(function (err, ports) {
+    ports.forEach(function(port) {
+      if (port.comName == comPort) {
+        win.webContents.send('avrdude', 'productId: ' + port.serialNumber);
+        resetPort(port.serialNumber);
+      }
+    });
   });
+})
 
-  avrdude.stderr.on('data', function (data) {
-    win.webContents.send('avrdude', data.toString());
-  });
+function resetPort(ID) {
+  win.webContents.send('avrdude', 'Attempting to connect to device 1200bps');
 
-  avrdude.on('exit', function (code) {
-    win.webContents.send('avrdude', code.toString());
-  });
+  const avrport = new serialport(comPort, {
+    baudRate: 1200
+  })
+
+  avrport.on('open', function() {
+    win.webContents.send('avrdude', 'Connected!');
+    setTimeout(() => {
+      win.webContents.send('avrdude', 'Closing port!');
+      avrport.close();
+    }, 100);
+
+    setTimeout(() => {
+      serialport.list(function (err, ports) {
+        ports.forEach(function(port) {
+          console.log(port.serialNumber);
+          
+          if (port.serialNumber == ID) {
+            win.webContents.send('avrdude', 'New port is: ' + port.comName);
+
+            win.webContents.send('avrdude', 'Running Avrdude...');
+            avrdude(port.comName, file);
+          }
+        });
+      });
+    }, 1000);
+  })
+
+  avrport.on('error', function(err) {
+    win.webContents.send('avrdude', err.toString());
+  })
 }
 
+function avrdude(port, file) { 
+  avrdude = spawn('./avrdude/avrdude.exe', ['-p', 'atmega32u4', '-C', './avrdude/avrdude.conf', '-c', 'avr109', '-b', '57600', '-D', '-P', port, '-U', 'flash:w:' + file + ':i']);
+
+      avrdude.stdout.on('data', function (data) {
+        win.webContents.send('avrdude', data.toString());
+      });
+    
+      avrdude.stderr.on('data', function (data) {
+        win.webContents.send('avrdude', data.toString());
+      });
+    
+      avrdude.on('exit', function (code) {
+        win.webContents.send('avrdude', code.toString());
+      });
+}
--- Scanner Pro MK3 Uploader/renderer.js
+++ Scanner Pro MK3 Uploader/renderer.js
@@ -1,24 +1,110 @@
 const ipc = require('electron').ipcRenderer;
+var serialport = require("serialport");
 
-const comPort = document.getElementById('comPort')
-const comButton = document.getElementById('comButton')
+document.addEventListener('DOMContentLoaded', pageLoaded);
+
+var com;
+var port;
+
 const fileButton = document.getElementById('file')
-const run = document.getElementById('run')
-
-comButton.addEventListener('click', () => {
-    ipc.send('comPort', comPort.value)
-})
-
 fileButton.addEventListener('click', () => {
     ipc.send('open-file-dialog-for-file')
 })
 
+const run = document.getElementById('run')
 run.addEventListener('click', () => {
     ipc.send('run')
 })
 
 var stdoutDiv = document.getElementById('stdout')
 ipc.on('avrdude', function (event, output) {
-    stdoutDiv.innerHTML += output
+    stdoutDiv.innerHTML += output + '<br>'
+    stdoutDiv.scrollTop = stdoutDiv.scrollHeight;
 });
 
+const serialData = document.getElementById('serial')
+const send = document.getElementById('send')
+send.addEventListener('click', () => {
+    var data = serialData.value
+    
+    port.write(data, function(err) {
+        if (err) {
+            return stdoutDiv.innerHTML += 'Error on write: ' + err.message
+        }
+    })
+})
+
+const connect = document.getElementById('connect')
+connect.addEventListener('click', () => {
+    port = new serialport(com, {
+        baudRate: 9600
+    })
+
+    port.on('data', function (data) {
+        stdoutDiv.innerHTML += data + "<br>"
+    })
+
+    port.on('error', function(err) {
+        stdoutDiv.innerHTML += err + "<br>"
+    })
+
+    port.on('open', function(err) {
+        stdoutDiv.innerHTML += "Successfully connected to device <br>"
+        serialData.disabled = false;
+        send.disabled = false;
+
+        connect.disabled = true;
+        close.disabled = false;
+    })
+
+    port.on('close', function(err) {
+        stdoutDiv.innerHTML += "Device disconnected! <br>"
+        serialData.disabled = true;
+        send.disabled = true;
+
+        connect.disabled = false;
+        close.disabled = true;
+    })
+
+})
+
+const close = document.getElementById('close')
+close.addEventListener('click', () => {
+    port.close()
+})
+
+var option = document.createElement("option");
+const update = document.getElementById('update')
+update.addEventListener('click', () => {
+    option.length = 0;
+    serialport.list(function (err, ports) {
+        ports.forEach(function(port) {
+            var text = port.comName + " - " + port.manufacturer;
+            option.text = text;
+            selectCom(text);
+            comPort.add(option);
+        });
+    });
+})
+
+const comPort = document.getElementById('comPort')
+function pageLoaded() {
+    serialData.disabled = true;
+    send.disabled = true;
+    close.disabled = true;
+
+    serialport.list(function (err, ports) {
+        ports.forEach(function(port) {
+            var text = port.comName + " - " + port.manufacturer;
+            option.text = text;
+            selectCom(text);
+            comPort.add(option);
+        });
+    });
+ }
+
+ function selectCom(port) {
+     var comParse = port.split(" -");
+     ipc.send('comPort', comParse[0])
+     com = comParse[0];
+ }
