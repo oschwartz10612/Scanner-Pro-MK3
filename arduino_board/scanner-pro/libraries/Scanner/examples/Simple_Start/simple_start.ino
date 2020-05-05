de999bb0cb5cadebe4f40d58dfc5b74281d6543a
Owen Schwartz <oschwartz10612@gmail.com>
2020-05-05 11:27:36 -0400

    Update simple_start.ino

--- arduino_board/scanner-pro/libraries/Scanner/examples/Simple_Start/simple_start.ino
+++ arduino_board/scanner-pro/libraries/Scanner/examples/Simple_Start/simple_start.ino
@@ -1,6 +1,7 @@
-#include <Keyboard.h>
+  
+#include <Scanner.h>
 
-
+Scanner Scanner;
 
 String data;
 
@@ -11,6 +12,14 @@ void setup() {
 void loop() {
   //Put your code here to loop
   
+  if(Scanner.available()) { //Check if there is data to read from the scanner
+    
+    data = Scanner.read_enter(); //Read the scanner data into a string. Use Scanner.read_tab() if your device uses tabs.
 
+    //Example munipulation
+    Scanner.print(data); //Print the scanner data
+    Scanner.write(10); //Send a new line
+    Scanner.print("END"); //Print the text "END"
+  }
   
 }
